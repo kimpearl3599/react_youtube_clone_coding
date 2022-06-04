a3774089f0de834cd7a76d99940a064e7cae9b4d
kimpearl3599 <kimpearl3599@gmail.com>
2022-06-04 19:32:18 +0900

    video_list postCSS styling

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+//props를 반복하고 싶지 않으면 props => {video}로 지정 가능
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.medium.url}
+        alt="video_thumbnail"
+      />
+      <div className={styles.metaData}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
 export default VideoItem;
